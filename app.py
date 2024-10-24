ccbb8e19e678b2f5d04e4920b5b5842994bca766
0xadrian <adrian.lardies@gmail.com>
2024-10-24 08:54:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ db_host = os.getenv('DB_HOST')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_name = os.getenv('DB_NAME')
-db_port = os.getenv('DB_PORT')
+db_port = os.getenv('DB_PORT', 3306)
 
 # Connect to MySQL database using the loaded environment variables
 conn = mysql.connector.connect(
